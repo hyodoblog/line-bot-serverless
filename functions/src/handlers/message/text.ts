8d4b55072e970129b0737b4775412458f51024dc
yusei-hyodo <yusei.hyodo@gmail.com>
2020-07-11 09:58:23 +0900

    オウム返しの実装

--- functions/src/handlers/message/text.ts
+++ functions/src/handlers/message/text.ts
@@ -1,7 +1,9 @@
-import { Line } from '../../line.config'
+import { Line, client } from '../../line.config'
+import { initUser } from '../../lib'
 import { datastoreGetFindBy } from '../../lib/gcloud/datastore'
 import { dsKindUser } from '../../models'
 import { User } from '../../models/user'
+import { makeReplyMessages } from '../../lib/line'
 // import * as status from '../../status'
 
 export const text = async (event: Line.MessageEvent): Promise<string> => {
@@ -11,7 +13,13 @@ export const text = async (event: Line.MessageEvent): Promise<string> => {
     event.source.userId
   )
 
-  console.log(user)
+  if (!user) {
+    await initUser(event, user)
+    return 'ユーザーデータを初期化'
+  }
 
-  return ''
+  const { text } = event.message as Line.TextMessage
+  await client.replyMessage(event.replyToken, makeReplyMessages(text))
+
+  return 'オウム返し'
 }
